3bf84f449d3fe05dfe4dc8a5c165908fa9e08cc7
David Porosnicu <david94_p@yahoo.com>
2019-02-13 12:01:21 +0200

    Initial changes & add Movie Card comp

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className="ui container"><MovieCard /></div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render(){
+    return(
+      <div>
+        <h2>Title</h2>
+        <p>Description</p>
+        <button>View</button>
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
